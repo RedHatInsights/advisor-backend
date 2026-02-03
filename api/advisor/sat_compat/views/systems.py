8001cb02cc140cd870c189015472515af0acd090
Paul Wayper <paulway@redhat.com>
2026-02-03 11:15:32 +1100

    RHINENG-23673: sat-compat patch crashes on system not found (#95)
    Systems should return a 404 if the UUID doesn't exist, a 200 otherwise
    (even if the call to Inventory fails).
    
    Signed-off-by: Paul Wayper <paulway@redhat.com>

--- api/advisor/sat_compat/views/systems.py
+++ api/advisor/sat_compat/views/systems.py
@@ -557,11 +557,13 @@ class V1SystemViewSet(viewsets.ReadOnlyModelViewSet):
         that the system being updated is actually the system which has that
         hostname. This is best-effort to support compatibility. The official
         way is to set the display_name in the upload or set it using
-        the inventory service. Always return a 200 no matter what.
+        the inventory service.  This returns a 404 if the system doesn't exist,
+        or a 200 whether or not the actual PATCH to inventory was successful.
         """
+        # Make sure the system exists in this org before we try to update it.
+        org_id = request_to_org(request)
+        system = get_system_or_404(self.get_queryset(), uuid, org_id)
         try:
-            org_id = request_to_org(request)
-            system = get_system_or_404(self.get_queryset(), uuid, org_id)
             # Validate form
             store_post_data(request, SatSystemEditSerializer)
             serdata = SatSystemEditSerializer(data=request.data)
@@ -580,7 +582,7 @@ class V1SystemViewSet(viewsets.ReadOnlyModelViewSet):
             else:
                 logger.info(f'Inventory returned ${response.status_code} on PATCH host')
         except (Exception,):
-            logger.exception('Failed to PATCH display_name')
+            logger.error('Failed to PATCH display_name')
 
         # System may not be created at this point.
         # The display_name will be set when the archive hits the inventory so always return 200.
