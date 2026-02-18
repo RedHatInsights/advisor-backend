2f1fd4e1de03936b9c7be5f319f861e3e994012d
Mark Huth <mhuth@redhat.com>
2026-02-19 08:33:18 +1000

    fix(RHINENG-23928): Use regex for matching RHEL versions (#103)

--- api/advisor/api/filters.py
+++ api/advisor/api/filters.py
@@ -546,14 +546,8 @@ required_branch_id_param = OpenApiParameter(
 rhel_version_query_param = OpenApiParameter(
     name='rhel_version', location=OpenApiParameter.QUERY,
     description='Display only systems with these versions of RHEL',
-    required=False, many=True, type=OpenApiTypes.STR, style='form',
-    enum=(
-        '10.0', '10.1', '10.2',
-        '9.0', '9.1', '9.2', '9.3', '9.4', '9.5', '9.6', '9.7', '9.8',
-        '8.0', '8.1', '8.2', '8.3', '8.4', '8.5', '8.6', '8.7', '8.8', '8.9', '8.10',
-        '7.0', '7.1', '7.2', '7.3', '7.4', '7.5', '7.6', '7.7', '7.8', '7.9', '7.10',
-        '6.0', '6.1', '6.2', '6.3', '6.4', '6.5', '6.6', '6.7', '6.8', '6.9', '6.10',
-    )
+    required=False, many=True, type=OpenApiTypes.REGEX, style='form',
+    pattern=r'^\d+\.\d+$'
 )
 
 
