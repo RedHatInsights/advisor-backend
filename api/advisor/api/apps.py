95b1ba7084101f72c6cd472e86ecdbca91c3f9a1
Mark Huth <mhuth@redhat.com>
2026-07-27 16:58:17 +1000

    fix(RHINENG-28754): Add custom inventory prometheus metrics to Django metrics (#201)
    - Plus create prometheus service in localdev environment

--- api/advisor/api/apps.py
+++ api/advisor/api/apps.py
@@ -19,3 +19,7 @@ from django.apps import AppConfig
 
 class ApiConfig(AppConfig):
     name = 'api'
+
+    def ready(self):
+        # include additional metrics from prometheus.py
+        import prometheus  # noqa: F401
--- api/advisor/prometheus.py
+++ api/advisor/prometheus.py
@@ -16,98 +16,26 @@
 # You should have received a copy of the GNU General Public License along
 # with Insights Advisor. If not, see <https://www.gnu.org/licenses/>.
 
-from django.conf import settings
-from prometheus_client import start_http_server, Counter, Enum, Gauge, Histogram, Info
+from prometheus_client import Counter
 
 # Prometheus configuration
-INSIGHTS_ADVISOR_UP = Gauge('insights_advisor_service_up', 'Insights Advisor Service Up')
-INSIGHTS_ADVISOR_STATUS = Enum(
-    'insights_advisor_service_status',
-    'Current Status of Advisor Service',
-    states=['initialized', 'starting', 'running', 'stopped']
-)
-INSIGHTS_ADVISOR_TOTAL_REQUESTS = Counter(
-    'insights_advisor_service_total_requests',
-    'Total Number of Received Requests'
-)
-INSIGHTS_ADVISOR_SUCCESSFUL_REQUESTS = Counter(
-    'insights_advisor_service_successful_requests',
-    'Total Number of Successful Requests'
-)
-INSIGHTS_ADVISOR_MISSING_CONTENT = Counter(
-    'insights_advisor_missing_content',
-    'Number of reports we tried processing but failed to find rule content for'
-)
-INSIGHTS_ADVISOR_DB_ERRORS = Counter(
-    'insights_advisor_db_errors',
-    'Total number of DB errors that have occurred'
-)
-INSIGHTS_ADVISOR_SERVICE_DB_ELAPSED = Histogram(
-    'insights_advisor_service_db_elapsed',
-    'Total time spent processing db calls for an archive'
-)
-THIRD_PARTY_RULE_HIT_REQUEST_RECEIVED = Counter(
-    'insights_advisor_service_third_party_rule_hit_request_received',
-    'A request that is received for a rule hit from a third party'
-)
-INSIGHTS_ADVISOR_SERVICE_HANDLE_ENGINE_RESULTS = Histogram(
-    'insights_advisor_service_handle_engine_results',
-    'Total time spent processing results received from the shared engine'
-)
-INSIGHTS_ADVISOR_SERVICE_RULE_HITS_ELAPSED = Histogram(
-    'insights_advisor_service_rule_hits_elapsed',
-    'Total time spent processing third party rule hits'
-)
-INSIGHTS_ADVISOR_SERVICE_WEBHOOK_EVENTS_ELAPSED = Histogram(
-    'insights_advisor_service_webhook_events_elapsed',
-    'Total time spent processing webhook events'
-)
-THIRD_PARTY_RULE_HIT_MISSING_KEYS = Counter(
-    'insights_advisor_service_rule_hits_missing_keys',
-    'Counter for how many rule hit requests are malformed, missing keys'
-)
-INVENTORY_EVENTS_TOPIC_RECEIVED = Counter(
-    'insights_advisor_service_inventory_event_received',
-    'A request that is received from the inventory service'
-)
-INSIGHTS_ADVISOR_SERVICE_INVENTORY_EVENTS_ELAPSED = Histogram(
-    'insights_advisor_service_inventory_events_elapsed',
-    'Total time spent processing an inventory event'
-)
-
 INVENTORY_EVENT_MISSING_KEYS = Counter(
-    'insights_advisor_service_inventory_event_missing_keys',
+    'insights_advisor_api_inventory_event_missing_keys',
     'Counter for how many inventory event requests are malformed, missing keys'
 )
-INVENTORY_EVENT_ERROR = Counter(
-    'insights_advisor_service_inventory_event_error',
-    'Counter for how many inventory events errored'
-)
 INVENTORY_EVENT_MALFORMED = Counter(
-    'insights_advisor_service_inventory_event_malformed',
+    'insights_advisor_api_inventory_event_malformed',
     'Counter for inventory events that failed validation or parsing'
 )
 INVENTORY_HOST_UPSERTED = Counter(
-    'insights_advisor_service_inventory_host_upserted',
+    'insights_advisor_api_inventory_host_upserted',
     'Count how many inventory hosts were upserted'
 )
 INVENTORY_HOST_DELETED = Counter(
-    'insights_advisor_service_inventory_host_deleted',
+    'insights_advisor_api_inventory_host_deleted',
     'Count how many inventory hosts were deleted'
 )
 INVENTORY_HOST_DELETE_MISSING = Counter(
-    'insights_advisor_service_inventory_host_delete_missing',
+    'insights_advisor_api_inventory_host_delete_missing',
     'Count how many inventory host delete events had no matching record in the DB'
 )
-PAYLOAD_TRACKER_DELIVERY_ERRORS = Counter(
-    'insights_advisor_service_payload_tracker_delivery_errors',
-    'Counter for how many payload tracker messsages failed delivery'
-)
-ADVISOR_SERVICE_VERSION = Info(
-    'insights_advisor_service_version',
-    'Release and versioning information'
-)
-
-
-def start_prometheus():
-    start_http_server(settings.PROMETHEUS_PORT)
