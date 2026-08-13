3e91ddfef9ca4ac6e1a5413185061e9d08a8e702
Mark Huth <mhuth@redhat.com>
2026-08-13 11:55:33 +1000

    Fix failing unittests

--- api/advisor/tasks/tests/test_executed_task_views.py
+++ api/advisor/tasks/tests/test_executed_task_views.py
@@ -682,10 +682,10 @@ class ExecutedTaskViewTestCase(TestCase):
         )
         self.assertEqual(res.status_code, 400)
         self.assertEqual(res.json(),
-            {'parameters': {'0': {
+            {'parameters': [{
                 'key': ['This field is required.'],
                 'value': ['This field is required.']
-            }}}
+            }]}
         )
         # Value not supplied
         res = self.client.post(
@@ -700,9 +700,9 @@ class ExecutedTaskViewTestCase(TestCase):
         )
         self.assertEqual(res.status_code, 400)
         self.assertEqual(res.json(),
-            {'parameters': {'0': {
+            {'parameters': [{
                 'value': ['This field is required.']
-            }}}
+            }]}
         )
 
         # Value not in list
