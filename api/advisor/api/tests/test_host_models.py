6a08e40200ca679d748324ef7395340bb248dc4c
Joao Paulo Ramos <jramos@redhat.com>
2026-09-04 18:17:02 -0300

    refactor(RHINENG-23281): remove Cyndi inventory model

--- api/advisor/api/tests/test_host_models.py
+++ api/advisor/api/tests/test_host_models.py
@@ -14,9 +14,13 @@
 # You should have received a copy of the GNU General Public License along
 # with Insights Advisor. If not, see <https://www.gnu.org/licenses/>.
 
-from django.test import TestCase
+from datetime import timedelta
 
-from api.models import Host
+from django.db import models
+from django.test import TestCase
+from django.utils import timezone
+
+from api.models import AdvisorInventoryHost, CurrentReport, Host, stale_systems_q
 from api.tests import constants
 
 
@@ -31,3 +35,43 @@ class HostModelTestCase(TestCase):
         self.assertEqual(h.account, '1234567')
         self.assertEqual(h.rhel_version, '7.5')
         self.assertEqual(str(h), constants.host_03_uuid)
+
+    def test_inventory_id_is_system_uuid_primary_key(self):
+        field = Host._meta.get_field('inventory_id')
+
+        self.assertIsInstance(field, models.UUIDField)
+        self.assertIs(Host._meta.pk, field)
+        self.assertEqual(field.column, 'system_uuid')
+        self.assertFalse(field.is_relation)
+        self.assertFalse(field.serialize)
+
+    def test_stale_systems_q_uses_org_and_inventory_id(self):
+        report = CurrentReport.objects.filter(
+            org_id=constants.standard_org,
+        ).first()
+        inventory_host = AdvisorInventoryHost.objects.get(
+            org_id=constants.standard_org,
+            inventory_id=report.host_id,
+        )
+        inventory_host.per_reporter_staleness = {
+            'puptoo': {
+                'stale_warning_timestamp': str(timezone.now() + timedelta(days=1)),
+            },
+        }
+        inventory_host.save(update_fields=['per_reporter_staleness'])
+
+        reports = CurrentReport.objects.filter(
+            stale_systems_q(constants.standard_org),
+            pk=report.pk,
+        )
+        _, params = reports.query.sql_with_params()
+        self.assertIn(constants.standard_org, params)
+        self.assertTrue(reports.exists())
+
+        inventory_host.per_reporter_staleness = {
+            'puptoo': {
+                'stale_warning_timestamp': str(timezone.now() - timedelta(days=1)),
+            },
+        }
+        inventory_host.save(update_fields=['per_reporter_staleness'])
+        self.assertFalse(reports.exists())
